48ff384f193787e5895af59176ca7aac9818cbb0
agapinank <agapinank@gmail.com>
2020-06-24 16:20:05 -0700

    display updates
    Signed-off-by: agapinank <agapinank@gmail.com>

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
-let pwChoices= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+let upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let lowerCase"abcdefghijklmnopqrstuvwxyz"
+0123456789
 let password =" ";
 let passwordText = document.getElementById('#password'); //#password id is a placeholder area to print pw
 
@@ -8,7 +10,7 @@ function writePassword() {
  let password = generatePassword();
  
 
- for (i = 0; i < array.length; index++) {
+ for (i = 0; i < array.length; i++) {
    
    
  }
