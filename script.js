c4aafa06c2a57bf5d9d7b9b654a260ed5d62de0c
agapinank <agapinank@gmail.com>
2020-07-15 20:24:08 -0700

    commit before submit
    Signed-off-by: agapinank <agapinank@gmail.com>

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-//variable assigned to document method set to return selected element ID
-const resultEl = document.getElementById('results');
-const lengthEl = document.getElementById('length');
-const upperCaseEl = document.getElementById('uppercase');
-const lowerCaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
+//variable assigned to document method set to return selected element
+const resultDiv = document.getElementById('result');
+const length = document.getElementById('length');
+const upperCase = document.getElementById('uppercase');
+const lowerCase = document.getElementById('lowercase');
+const number = document.getElementById('numbers');
+const symbols = document.getElementById('symbols');
+const generate = document.getElementById('generate');
 const clipboard = document.getElementById('clipboard');
 
-// variable assigned object.properties value random functions
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
-
-//on click clipboard btn
 clipboard.addEventListener('click', () => {
   const textarea = document.createElement('textarea');
-  const password = resultEl.innerText();
+  const password = resultDiv.innerText;
 
   if (!password) {
     return;
@@ -30,53 +27,64 @@ clipboard.addEventListener('click', () => {
   textarea.select();
   document.execCommand('copy');
   textarea.remove();
-  alert('Password copied to clipboard');
+  alert('Password copied');
 });
 
 generate.addEventListener('click', () => {
-  const length = lengthEl.value;
-  const hasLower = lowerCaseEl.checked;
-  const hasUpper = upperCaseEl.checked;
-  const hasNumber = numbers.checked;
+  const hasLength = +length.value;
+  const hasLower = lowerCase.checked;
+  const hasUpper = upperCase.checked;
+  const hasNumber = number.checked;
   const hasSymbol = symbols.checked;
 
-  resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, length);
+resultDiv.innerText = generatePassword(
+    hasLength,
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol
+  );
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
-  let generatedPassword = ' ';
-  const typesCount = lower + upper + number + symbol;
-  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+  let randomPassword = ' ';
+  const typeCount = lower + upper + number + symbol;
+
+  const typeArr = [{ lower }, { upper }, { number }, { symbol }].filter(
     item => Object.values(item)[0]
   );
-
-  //does not have a selected type will return empty string
-  if (typesCount === 0) {
-    return ' ';
+  if (typeCount === 0) {
+    return '';
   }
-  //  loop thru the length of typesArr and incr||equal to typesCount
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach(type => {
+  for (let i = 0; i <= length; i += typeCount) {
+    typeArr.forEach(type => {
       const funcName = Object.keys(type)[0];
-      generatedPassword += randomFunc[funcName]();
+
+      randomPassword += randomFunc[funcName]();
     });
   }
-
-  const finalPassword = generatedPassword.slice(0, length);
-
-  return finalPassword;
+  const generateSolution = randomPassword.slice(0, length);
+  return generateSolution;
 }
 
+//Random function creation
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+console.log(getRandomLower);
+
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+console.log(getRandomUpper);
+
 function getRandomNumber() {
-  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+console.log(getRandomNumber);
+
 function getRandomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.';
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
+console.log(getRandomSymbol);
